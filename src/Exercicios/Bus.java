a93e7c38a76817e3bbfd7c117ecd117de4a0df5e
juniormagri <juniormagrix@gmail.com>
2022-07-28 20:14:22 -0300

    Exercicios

---
+++ src/Exercicios/Bus.java
@@ -0,0 +1,13 @@
+package Exercicios;
+
+public class Bus extends Vehicles {
+
+    public Bus(String model, String color, double value) {
+        super(model, color, value);
+    }
+
+    String Venda(){
+        return ("Onibus a venda por: ");
+
+    }
+}
